f101cd7668e8bf881b28b5d438aa7a1c1646707e
Amalia Bryant <amaliacbryant@gmail.com>
2021-08-11 23:38:30 -0700

    added ETL plan to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SDC-QA
+
+## Plans for ETL
+* Creating the table structure
+* Importing from a psql prompt
+* Importing from a shell prompt
+* Exporting data to a .csv file
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-//amazing code here
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const db = require('./queries');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+//this doesn't work yet
+// app.get('/questions', db.getQuestions);
+
+app.get('/', (req, res)=>{
+  res.json({info: 'Node.js, Express, and Postgres API'})
+});
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
